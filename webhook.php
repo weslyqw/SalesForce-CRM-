3b461b39601275b6ec0e2ce75086a043373f8b49
Akel Nickles <akelqw@gmail.com>
2022-05-24 16:05:50 -0400

    This is the What Converts Web hook
    It captures all the data of a lead when created in What Converts

--- webhook.php
+++ webhook.php
@@ -4,13 +4,12 @@
 
     $salesForce = new SalesForce();
     $data = json_decode(file_get_contents('php://input'), true);
-    $dataFile = fopen("data.txt", "w");
 
     try {
             $leadId = intval($data["lead_id"]);
             $email = $data["email_address"];
             $getSalesForceToken = $salesForce->getSalesForceAccessToken();
-            $salesForceSearch = $salesForce->getSalesForceLeadSearch("12345sdfsf6@me.com",$leadId,$getSalesForceToken,$url);
+            $salesForceSearch = $salesForce->getSalesForceLeadSearch($email,$leadId,$getSalesForceToken);
             
             echo "Sales Force Token: " . $getSalesForceToken;
 
