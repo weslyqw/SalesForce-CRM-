5250252b2bc46febf6270be06f2a64285ecf5eee
Akel Nickles <akelqw@gmail.com>
2022-05-24 16:04:02 -0400

    Sales Force webhook

--- salesforceContactWebhook.php
+++ salesforceContactWebhook.php
@@ -5,9 +5,11 @@
     $data = json_decode(file_get_contents('https://echo-webhook.herokuapp.com/testWebhookTrigger'), true);
 
     try {
+        var_dump($data);
         $leadId = ($data['new'][0]['Description']);
-        $addational_field = "Connected";
-        $salesForceSearch = $salesForce->setWcSalesForceField($leadId,$addational_field);
+        $wcQuotable = "yes";
+        $getSalesForceToken = $salesForce->getSalesForceAccessToken();
+        $salesForce->setWcQuotable($wcQuotable,$leadId);
         } catch (\Throwable $th) {
             echo "Error / " . $th->getMessage();
         } 
